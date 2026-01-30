2f21db77d9262607a65c4516d2849c1cbc21c7a8
LV.1 <zone3577zone@gmail.com>
2026-01-30 06:35:46 +0000

    เพิ่มปุ่ม

--- a/app/page.tsx
+++ a/app/page.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import Header from "@/component/header";
 
 export default function Home() {
   return (
     <div className="w-full min-h-screen bg-zinc-50 font-sans dark:bg-black">
       <Header />
+      <div>
         <iframe
           src="https://my.spline.design/r4xbot-7DzALkHCjWC86AlwWp6THOEU/"
           className="w-full h-screen"
           allow="autoplay; fullscreen"
+          loading="lazy"
         />
+      </div>
     </div>
   );
 }
---
+++ a/component/leftbutton.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react'
+
+function leftbutton() {
+  return (
+    <div>leftbutton</div>
+  )
+}
+
+export default leftbutton
